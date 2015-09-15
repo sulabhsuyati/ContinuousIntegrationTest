5c711524c80e92aac3f18e4ac654d380cc4a816e
Sulabh <ssurendran@suyati.com>
2015-09-15 18:40:04 +0530

    Added Project

---
+++ MyIntegrationTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyIntegrationTester
+//
+//  Created by User1 on 9/15/15.
+//  Copyright (c) 2015 Suyati Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyIntegrationTester/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyIntegrationTester
+//
+//  Created by User1 on 9/15/15.
+//  Copyright (c) 2015 Suyati Technologies. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
